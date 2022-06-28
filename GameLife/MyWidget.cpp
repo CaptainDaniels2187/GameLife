09263785ebac5cc33301981a8298ff278b7506dc
Даниил Бутенин <butenin.dyu@edu.spbstu.ru>
2022-06-28 12:46:52 +0300

    Correct draw left rect

--- GameLife/MyWidget.cpp
+++ GameLife/MyWidget.cpp
@@ -1,9 +1,15 @@
 #include "MyWidget.h"
+#include <QDebug>
+
+#define FIELD_RIGHT_MARGIN 50
+#define FIELD_BOTTOM_MARGIN 20
+
+
 
 void MyWidget::paintEvent(QPaintEvent* event)
 {
 	QPainter painter(this);
-	painter.setPen(QPen(Qt::black, 12));
-	qDebug() << this->pos() << " | " << this->pos() + QPoint(this->width(), this->height());
-	painter.drawRect(QRect(this->pos(), this->pos() + QPoint(this->width(), this->height())));
+	painter.setPen(QPen(Qt::black, 2));
+	qDebug() << this->pos() << " | " << this->pos() + QPoint(this->width() - FIELD_RIGHT_MARGIN, this->height() - FIELD_BOTTOM_MARGIN);
+	painter.drawRect(QRect(this->pos(), this->pos() + QPoint(this->width() - FIELD_RIGHT_MARGIN, this->height() - FIELD_BOTTOM_MARGIN)));
 }
