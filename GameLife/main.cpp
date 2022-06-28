4e8163332f95aa15586581a31595d1fc3883cdcc
Даниил Бутенин <butenin.dyu@edu.spbstu.ru>
2022-06-28 13:47:33 +0300

    Localization and correct open size

--- GameLife/main.cpp
+++ GameLife/main.cpp
@@ -10,24 +10,28 @@ int main(int args, char** argv)
 	QApplication GameLife(args, argv);
 	
 	QWidget MainWindow;
+	MainWindow.setGeometry(0, 0, 1280, 720);
 
 	MyWidget* TestRect = new MyWidget;
 
-	QPushButton* pb1 = new QPushButton("1");
+	QPushButton* pb1 = new QPushButton(QString(QString::fromWCharArray(L"Старт")));
 	pb1->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
-	QPushButton* pb2 = new QPushButton("2");
+	QPushButton* pb2 = new QPushButton(QString(QString::fromWCharArray(L"Продолжить")));
 	pb2->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
-	QPushButton* pb3 = new QPushButton("3");
+	QPushButton* pb3 = new QPushButton(QString(QString::fromWCharArray(L"Пауза")));
 	pb3->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
-	QPushButton* pb4 = new QPushButton("4");
+	QPushButton* pb4 = new QPushButton(QString(QString::fromWCharArray(L"Сброс")));
 	pb4->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
-	QPushButton* pb5 = new QPushButton("5");
+	QPushButton* pb5 = new QPushButton(QString(QString::fromWCharArray(L"Случайная расстановка")));
 	pb5->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
-	QPushButton* pb6 = new QPushButton("6");
+	QPushButton* pb6 = new QPushButton(QString(QString::fromWCharArray(L"Выход")));
 	pb6->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
 
 	QHBoxLayout* hlayout = new QHBoxLayout;
 	QVBoxLayout* vlayout = new QVBoxLayout;
+	vlayout->setContentsMargins(0, 15, 30, 15);
+	vlayout->setSpacing(15);
+	//vlayout->
 
 	hlayout->addWidget(TestRect);
 	vlayout->addWidget(pb1);
@@ -35,6 +39,7 @@ int main(int args, char** argv)
 	vlayout->addWidget(pb3);
 	vlayout->addWidget(pb4);
 	vlayout->addWidget(pb5);
+	vlayout->addStretch();
 	vlayout->addWidget(pb6);
 	hlayout->addLayout(vlayout);
 
